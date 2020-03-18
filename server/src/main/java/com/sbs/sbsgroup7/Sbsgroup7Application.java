02fbaed4f0b0f9b4b015deeee8ed82938856ac86
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2020-03-18 11:57:24 -0700

    add certificate and https

--- server/src/main/java/com/sbs/sbsgroup7/Sbsgroup7Application.java
+++ server/src/main/java/com/sbs/sbsgroup7/Sbsgroup7Application.java
@@ -2,6 +2,7 @@ package com.sbs.sbsgroup7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Sbsgroup7Application {
