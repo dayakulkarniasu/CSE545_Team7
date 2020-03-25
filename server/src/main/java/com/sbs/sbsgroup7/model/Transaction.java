8e45c74b45b5e3a28ad1bced77b5b6709a4cd862
Daya Kulkarni <dkulkar2@asu.edu>
2020-03-25 15:10:04 -0700

    transaction columns

--- server/src/main/java/com/sbs/sbsgroup7/model/Transaction.java
+++ server/src/main/java/com/sbs/sbsgroup7/model/Transaction.java
@@ -23,13 +23,72 @@ public class Transaction {
     @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String transactionID;
 
+    @Column(name = "amount")
+    @NotNull
+    private double amount;
 
-    public  Transaction(@JsonProperty("transactionID") String transactionID){
+    @Column(name = "createdTime")
+    @NotNull
+    private int createdTime;
+
+    @Column(name = "status")
+    @NotNull
+    private String status;
+
+    @Column(name = "commitTime")
+    @NotNull
+    private int commitTime;
+
+    @Column(name = "description")
+    @NotNull
+    private String description;
+
+    public  Transaction(@JsonProperty("transactionID") String transactionID,
+                        @JsonProperty("transactionID") double amount,
+                        @JsonProperty("transactionID") int createdTime,
+                        @JsonProperty("transactionID") String status,
+                        @JsonProperty("transactionID") int commitTime,
+                        @JsonProperty("transactionID") String description){
         this.transactionID  =transactionID;
+        this.amount  =amount;
+        this.createdTime  =createdTime;
+        this.status  =status;
+        this.commitTime  =commitTime;
+        this.description  =description;
 
     }
 
     public String getTID() { return transactionID; }
+
+    public void setTID(String transactionID){
+        this.transactionID=transactionID;
+    }
+    public double getAmount() { return amount; }
+
+    public void setTID(double amount){
+        this.amount=amount;
+    }
+    public double getCreateTime() { return createdTime; }
+
+    public void setCreatedTime(int createdTime){
+        this.createdTime=createdTime;
+    }
+    public int getCommitTime (){ return commitTime; }
+
+    public void setCommitTime(int commitTime){
+        this.commitTime=commitTime;
+    }
+    public String getStatus(){ return status; }
+
+    public void setStatus(String status){
+        this.status=status;
+    }
+
+    public String getDescription(){ return description; }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
 
 
 }
