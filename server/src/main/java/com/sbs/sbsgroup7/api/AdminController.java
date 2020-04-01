defb397cd7f157c9fdf44e2edd2b19a46a13c3c6
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2020-03-31 19:56:35 -0700

    add view, update, delete employee account

--- server/src/main/java/com/sbs/sbsgroup7/api/AdminController.java
+++ server/src/main/java/com/sbs/sbsgroup7/api/AdminController.java
@@ -2,14 +2,20 @@ package com.sbs.sbsgroup7.api;
 
 import com.sbs.sbsgroup7.DataSource.EmployeeUpdatesRepository;
 import com.sbs.sbsgroup7.DataSource.SystemLogRepository;
+import com.sbs.sbsgroup7.DataSource.UserRepository;
+import com.sbs.sbsgroup7.model.Request;
 import com.sbs.sbsgroup7.model.SystemLog;
+import com.sbs.sbsgroup7.model.User;
 import com.sbs.sbsgroup7.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,6 +29,9 @@ public class AdminController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/home")
     public String adminHome() {
@@ -51,5 +60,60 @@ public class AdminController {
         employeeUpdatesRepository.deleteById(userId);
 
         return "redirect:technicalAccountAccess";
+    }
+
+    @RequestMapping("/manageAccounts")
+    public String manageAccounts(Model model) {
+        Stream<User> employeeList1 = userService.findAll().stream()
+                .filter(e -> e.getRole().equals("TIER1"));
+        Stream<User> employeeList2 = userService.findAll().stream()
+                .filter(e -> e.getRole().equals("TIER2"));
+        List<User> employeeList = Stream.concat(employeeList1, employeeList2)
+                .collect(Collectors.toList());
+
+        model.addAttribute("employees", employeeList);
+
+        return "admin/manageAccounts";
+    }
+
+    @RequestMapping(value = "/manageAccounts/{userId}", method = RequestMethod.GET)
+    public String manageAccount(@PathVariable("userId") String userId, Model model) {
+        User user = userService.findByUserId(userId);
+
+        model.addAttribute("user", user);
+
+        return "admin/updateEmployeeProfile";
+    }
+
+    @PostMapping("/manageAccount")
+    public String manageAccount(@RequestParam("userId") String userId, @RequestParam("action") String action) {
+        if (action.equals("delete")) {
+            userService.delete(userId);
+            return "redirect:manageAccounts";
+        } else {
+            return "redirect:manageAccounts/" + userId;
+        }
+    }
+
+
+    @PostMapping("/updateEmployeeProfile")
+    public String updateEmployeeProfile(@RequestParam("firstName") String firstName,
+                                        @RequestParam("lastName") String lastName,
+                                        @RequestParam("email") String email,
+                                        @RequestParam("ssn") String ssn,
+                                        @RequestParam("address") String address,
+                                        @RequestParam("phone") String phone,
+                                        @RequestParam("userId") String userId) {
+
+        User user = userRepository.findByUserId(userId);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setSsn(ssn);
+        user.setAddress(address);
+        user.setPhone(phone);
+        userRepository.save(user);
+
+        return "redirect:manageAccounts";
     }
 }
